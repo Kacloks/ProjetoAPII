a98f2f6336756d57f0c3c51646ec93ffba6916ed
Kacloks <117231721+Kacloks@users.noreply.github.com>
2024-09-23 18:46:03 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        Funcionario funcionario = new Funcionario("João", 30, TipoDocumento.CPF, "Gerente");
-        Cliente cliente = new Cliente("Maria", 25, TipoDocumento.rg, "Eletrônicos");
+        try {
+            Funcionario funcionario = new Funcionario("João", 30, TipoDocumento.CPF, "Gerente");
+            Cliente cliente = new Cliente("Maria", 25, TipoDocumento.rg, "Eletrônicos");
 
        
-        System.out.println("Informações do Funcionário:");
-        funcionario.exibirInformacoes();
+            System.out.println("Informações do Funcionário:");
+            funcionario.exibirInformacoes();
 
-        System.out.println("\nInformações do Cliente:");
-        cliente.exibirInformacoes();
+            System.out.println("\nInformações do Cliente:");
+            cliente.exibirInformacoes();
+
+            Cliente clienteInvalido = new Cliente("Carlos", -5, TipoDocumento.RG, "Roupas");
+        } catch (IdadeInvalida e) {
+            systen.out.println("Erro:" + e.getMessage());
+        }
     }
 }
